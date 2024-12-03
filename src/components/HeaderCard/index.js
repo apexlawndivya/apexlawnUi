17c7027cf3f5e3cb6121e3009dbb07b83c36143a
apexlawndivya <djanga@apexlawncompany.com>
2024-12-03 13:19:38 +0530

    added header links

--- src/components/HeaderCard/index.js
+++ src/components/HeaderCard/index.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./headercard.module.css";
 
 function HeaderCard({ item }) {
-  const { type, text, image } = item || {};
+  const { type, text, image, path } = item || {};
   return (
-    <div className={`${styles.headerCard}`}>
+    <Link href={path} className={styles.headerCard}>
       {type === "text" ? (
         <p className={styles.headerName}>{text}</p>
       ) : (
@@ -20,7 +21,7 @@ function HeaderCard({ item }) {
           }}
         />
       )}
-    </div>
+    </Link>
   );
 }
 
--- src/data/header.js
+++ src/data/header.js
@@ -4,22 +4,27 @@ const HEADER_DATA = [
   {
     type: "text",
     text: "Lawncare",
+    path: "/lawncare",
   },
   {
     type: "text",
     text: "Irrigation",
+    path: "/Irrigation",
   },
   {
     type: "image",
     image: apexLawn,
+    path: "/"
   },
   {
     type: "text",
     text: "Landscaping",
+    path: "/landscaping",
   },
   {
     type: "text",
     text: "More Products",
+    path: "/more"
   },
 ];
 
